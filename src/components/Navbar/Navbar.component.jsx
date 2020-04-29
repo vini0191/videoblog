c4ac0e3ad4de9fc24ebe2b43bbed131eb2b86e2c
Vinicius Alvim <vini0191@gmail.com>
2020-04-29 15:06:37 -0300

    Small update

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { NavContainer, NavButton } from "./Navbar.style";
 
 const Navbar = () => (
-  <div className="container">
-    <NavContainer>
-      <NavButton>Primeiro item</NavButton>
-      <NavButton>Segundo item</NavButton>
-      <NavButton>Terceiro item</NavButton>
-    </NavContainer>
-  </div>
+  <NavContainer>
+    <NavButton>Primeiro item</NavButton>
+    <NavButton>Segundo item</NavButton>
+    <NavButton>Terceiro item</NavButton>
+  </NavContainer>
 );
 
 export default Navbar;
