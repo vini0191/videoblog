d2bd447489f59f37b9943a7757d216fa1a0cbd59
Vinicius Alvim <vini0191@gmail.com>
2020-05-19 17:57:56 -0300

    ongoing...

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,40 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { tags, videoLinks } from "../Main/videoLinksArray";
-import { NavContainer, HomeButton, NavButton } from "./Navbar.style";
+import {
+  NavContainer,
+  Ul,
+  MobButton,
+  HomeButton,
+  NavButton,
+} from "./Navbar.style";
+import { FaAlignRight } from "react-icons/fa";
 import logo from "../../assets/images/tlogo.jpg";
 
 const Navbar = ({ setVideos }) => {
+  const [toggle, setToggle] = useState(false);
   const filterTakedArray = (tag) =>
     [...videoLinks].reverse().filter((video) => video.tags.includes(tag));
+  // const heightConfig = { `height: ${toggle ? "120px" : 0}` };
 
   return (
-    <NavContainer>
+    <NavContainer className="navBar">
+      <MobButton onClick={() => setToggle(!toggle)}>
+        <FaAlignRight />
+      </MobButton>
+      {/* <Ul className={toggle ? "links show-nav" : "links"}> */}
       <HomeButton>
         <Link onClick={() => setVideos([...videoLinks].reverse())} to="/">
           <img src={logo} alt="Logo" />
           <h2>Memorial Corinthians</h2>
         </Link>
       </HomeButton>
-      <NavButton>
-        <Link onClick={() => setVideos(filterTakedArray(tags[0]))} to="/">
-          Campeonatos
-        </Link>
-      </NavButton>
-      <NavButton>
-        <Link onClick={() => setVideos(filterTakedArray(tags[1]))} to="/">
-          Jogadores
-        </Link>
-      </NavButton>
-      <NavButton>
-        <Link onClick={() => setVideos(filterTakedArray(tags[2]))} to="/">
-          Jogos Completos
-        </Link>
-      </NavButton>
-      <NavButton>
-        <Link to="/contato">Contato</Link>
-      </NavButton>
+      <Ul toggle={toggle}>
+        <NavButton>
+          <Link onClick={() => setVideos(filterTakedArray(tags[0]))} to="/">
+            Campeonatos
+          </Link>
+        </NavButton>
+        <NavButton>
+          <Link onClick={() => setVideos(filterTakedArray(tags[1]))} to="/">
+            Jogadores
+          </Link>
+        </NavButton>
+        <NavButton>
+          <Link onClick={() => setVideos(filterTakedArray(tags[2]))} to="/">
+            Jogos Completos
+          </Link>
+        </NavButton>
+        <NavButton>
+          <Link to="/contato">Contato</Link>
+        </NavButton>
+      </Ul>
     </NavContainer>
   );
 };
--- src/components/Navbar/Navbar.style.js
+++ src/components/Navbar/Navbar.style.js
@@ -1,44 +1,75 @@
 import styled from "styled-components";
 
 export const NavContainer = styled.div`
-  display: flex;
-  font-size: 20px;
+  width: 100%;
+  background-color: #111;
+  color: white;
+  padding: 10px;
 `;
 
-export const HomeButton = styled.button`
+export const MobButton = styled.button`
+  font-size: 30px;
+  margin-right: 30px;
+  color: white;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  float: right;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const Ul = styled.ul`
+  height: ${({ toggle }) => (toggle ? "200px" : "0px")};
+  overflow: hidden;
+  list-style-type: none;
+  font-weight: bold;
+  @media screen and (min-width: 768px) {
+    height: auto;
+    display: flex;
+  }
+`;
+
+export const HomeButton = styled.li`
   display: flex;
-  justify-content: space-evenly;
+  /* justify-content: space-evenly; */
   align-items: center;
   flex: 1 auto;
-  /*background: gray;*/
   background: #4e3272;
   font-size: 1em;
-  border-color: transparent;
-
-  a {
+  /* border-color: transparent; */
+  padding: 10px 0;
+  /* --------------------------------- */
+  /* a {
     display: flex;
     flex-direction: row;
-    /* justify-content: center; */
 
     img {
       width: 50%;
-      /* margin-right: 10px; */
     }
 
     h2 {
       align-self: center;
     }
+  } */
+  /* --------------------------------- */
+  @media screen and (min-width: 768px) {
+    padding: 0 15px;
   }
 `;
 
-export const NavButton = styled.button`
-  display: flex;
+export const NavButton = styled.li`
+  /* display: flex;
   justify-content: center;
   align-items: center;
-  /* flex: 33.33%; */
   flex: 20%;
-  /*background: gray;*/
   background: #4e3272;
   border-color: transparent;
-  font-size: 1.1em;
+  font-size: 1.1em; */
+  padding: 10px 0;
+  @media screen and (min-width: 768px) {
+    padding: 0 15px;
+  }
 `;
