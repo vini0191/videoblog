2ac168110a7874bc6152f7b3e36d80eb8fd997ad
Cesar Corregiari <cesarcorregiari@gmail.com>
2020-05-22 16:01:14 -0300

    footer ok

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { FootContainer, FootButton } from "./Footer.style";
 
 const Footer = () => (
   <FootContainer>
-    <FootButton>Primeiro item do footer</FootButton>
-    <FootButton>Segundo item do footer</FootButton>
-    <FootButton>Terceiro item do footer</FootButton>
+    <FootButton>
+      <Link to="/contato">Entre em Contato / Apoie o site</Link>
+    </FootButton>
+    <a
+      href="https://www.corinthians.com.br"
+      target="blank"
+      rel="noopener noreferrer"
+    >
+      Site criado por torcedores. Clique aqui para acessar o site oficial do
+      Corinthians.
+    </a>
   </FootContainer>
 );
 
